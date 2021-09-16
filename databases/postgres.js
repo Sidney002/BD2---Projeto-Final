f8876c68ce2eb29b4a83523b760764733477380e
Tales-Anderson <tales.anderson@academico.ifpb.edu.br>
2021-09-16 20:28:04 -0300

    Alterando conexoes

--- databases/postgres.js
+++ databases/postgres.js
@@ -1,57 +1,48 @@
-// require('dotenv').config();
-// const {Client} = require('pg');
-// //conectar ao banco
-// const client = new Client({
-//     host: process.env.PG_HOST,
-//     port: process.env.PG_PORT,
-//     database: process.env.PG_DATABASE,
-//     user: process.env.PG_USER,
-//     password: process.env.PG_PASSWORD
-// });
-// //conectar cliente
-// client.connect()
-//     .then(()=>console.log("Cliente Conectado"))
-//     .catch(err => console.log("err.stack"))
-
-    
-// async function setNewUser(nome,email, senha, nascimento){
-//     const newUser = "INSERT INTO crudblog (nome,email,senha,nascimento) VALUES ($1, $2, $3, $4)"
-//     client.query(newUser,[nome, email, senha, nascimento])
-//     console.log("usuario cadastrado com sucesso!")
-// }
-
-// async function getUser(email) {
-//     const res = await client.query(`SELECT * FROM crudblog WHERE email=$1`,[email])
-    
-
-//     if(res.rows.length > 0) {
-//         return res.rows[0]
-//     }else return null
-// }
-
-// async function getUserEmail(email) {
-//     const res = await client.query(`SELECT email FROM crudblog WHERE email=$1 LIMIT 1`,[email])
-    
-
-//     if(res.rows.length > 0) {
-//         return res.rows[0]
-//     }else return null
-// }
-
-// async function getUserPassword(email) {
-//     const res =  await client.query(`SELECT senha FROM crudblog WHERE email=$1 LIMIT 1`,[email])
-
-//     if(res.rows.length > 0) {
-//         return res.rows[0]
-//     }else return null
-// }
-
-
-// //exportar cliente
-// module.exports = {
-//     client,
-//     getUserEmail,
-//     getUserPassword,
-//     setNewUser,
-//     getUser
-// }
+ require('dotenv').config();
+ const {Client} = require('pg');
+ //conectar ao banco
+ const client = new Client({
+     host: process.env.PG_HOST,
+     port: process.env.PG_PORT,
+     database: process.env.PG_DATABASE,
+     user: process.env.PG_USER,
+     password: process.env.PG_PASSWORD
+ });
+ //conectar cliente
+ client.connect()
+     .then(()=>console.log("Cliente Conectado"))
+     .catch(err => console.log("err.stack"))
+  
+ async function setNewUser(nome,email, senha, nascimento){
+     const newUser = "INSERT INTO crudblog (nome,email,senha,nascimento) VALUES ($1, $2, $3, $4)"
+     client.query(newUser,[nome, email, senha, nascimento])
+     console.log("usuario cadastrado com sucesso!")
+ }
+ async function getUser(email) {
+     const res = await client.query(`SELECT * FROM crudblog WHERE email=$1`,[email])
+  
+     if(res.rows.length > 0) {
+         return res.rows[0]
+     }else return null
+ }
+ async function getUserEmail(email) {
+     const res = await client.query(`SELECT email FROM crudblog WHERE email=$1 LIMIT 1`,[email])
+  
+     if(res.rows.length > 0) {
+         return res.rows[0]
+     }else return null
+ }
+ async function getUserPassword(email) {
+     const res =  await client.query(`SELECT senha FROM crudblog WHERE email=$1 LIMIT 1`,[email])
+     if(res.rows.length > 0) {
+         return res.rows[0]
+     }else return null
+ }
+ //exportar cliente
+ module.exports = {
+     client,
+     getUserEmail,
+     getUserPassword,
+     setNewUser,
+     getUser
+ }
