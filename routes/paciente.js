57e29bd53c379d5cef3d1a773b141f1812d9f422
Sidney <caio.sidney69@gmail.com>
2021-09-16 19:44:45 -0300

    primeira parte dos templates e rotas

--- routes/paciente.js
+++ routes/paciente.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/', (req,res)=>{
-    res.render('base')
+    res.render('index')
 })
 
-router.get('/test', (req,res)=>{
-    res.render('index')
+router.get('/Entrar', (req,res)=>{
+    res.render('logar')
+})
+
+router.get('/Sobre', (req,res)=>{
+    res.render('sobre')
+})
+
+router.get('/Ver-UBS', (req,res)=>{
+    res.render('UBS')
+})
+router.get('/Cadastrar', (req,res)=>{
+    res.render('cadastrar')
 })
 
 module.exports = router
